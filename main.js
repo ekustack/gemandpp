cdb33de45e3772f67ed346ce4ada6c226b1e31f0
Figsh <safetyunion5550@gmail.com>
2025-08-28 10:01:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,218 @@
-import{initializeApp}from"https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import{getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword,onAuthStateChanged,signOut}from"https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import{getFirestore,collection,addDoc,onSnapshot,getDocs,query,where,orderBy,deleteDoc,doc,updateDoc,increment}from"https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-const e=await fetch("/.netlify/functions/verifyFb"),t=await e.json(),a=initializeApp(t),n=getAuth(a),o=getFirestore(a),i=collection(o,"points");
-let c=null;
-const u={image:document.querySelector(".re-user-img"),name:document.querySelector(".re-user-name"),pp:document.querySelector(".pp-count"),gm:document.querySelector(".gm-count"),ex:document.querySelector(".c-ex"),ppIcon:document.querySelector(".icon.pp"),gmIcon:document.querySelector(".gem"),getGem:document.querySelector(".gem"),getGift:document.querySelector(".gift"),claimCard:document.querySelector(".gift-claim"),claimGm:document.querySelector(".gm-claim"),uname:document.getElementById("uname"),uem:document.getElementById("uem"),tpp:document.getElementById("tpp"),tgm:document.getElementById("tgm")};
-onAuthStateChanged(n,(e=>{if(e){c=e;const t=c.photoURL;t?u.image.style.cssText=`background: url(${t}) no-repeat center; background-size: contain;`:u.image.textContent="U",c.displayName?u.image.textContent=`${c.displayName.slice(0,1)}`:u.image.textContent="U",u.name.textContent=`${c.displayName?c.displayName:`${c.id?"user_"+c.id.slice(0,5):"user_"}`}`,u.uem.value=c.email,u.uname.value=c.displayName,document.getElementById("refURL").value=`https://gemandpp.netlify.app/enter#ref?id=${encodeURIComponent(c.email)}`;const e=document.querySelector(".gift-count");e.innerText=u.claimCard.querySelectorAll(".card").length,u.getGift.addEventListener("click",(e=>{u.claimCard.style.display="block"})),u.gmIcon.addEventListener("click",(e=>{r(9,4),document.querySelector(".pay-for").style.display="block"})),u.claimCard.querySelectorAll(".card button").forEach((e=>{e.onclick=function(){e.style.opacity="50%",e.textContent="CLAIMED",d(1),e.innerText-=1,e.onclick=null,setTimeout((()=>{window.open("https://www.effectivecpmrate.com/uxx2gjrqc?key=76a628bbfc45e55ee2e31d6593b85938")}),300)}}));const t=document.querySelectorAll(".part-par");t.forEach((e=>{const t=e.querySelector(".part-1"),a=e.querySelector(".part-2"),n=e.querySelector(".part-cover");t.addEventListener("click",(e=>{n.style.marginLeft="0",n.style.borderRadius="10px 0 0 10px"})),a.addEventListener("click",(e=>{n.style.marginLeft="45%",n.style.borderRadius="0 10px 10px 0"}));const o=e.querySelectorAll(".pp-get"),i=e.querySelectorAll(".pp-price"),c=e.querySelectorAll(".gm-get"),r=e.querySelectorAll(".gm-price"),s=e.querySelectorAll(".get"),l=e.querySelectorAll(".get-gm"),d=e.querySelector(".buy-gem");s.forEach((e=>{e.addEventListener("click",(e=>{r(+e.parentElement.querySelector(".pp-get").innerText,e.parentElement.querySelector(".pp-price").innerText),document.querySelector(".pay-for").style.display="block"}))})),l.forEach((e=>{e.addEventListener("click",(e=>{r(7*+e.parentElement.querySelector(".gm-get").innerText,e.parentElement.querySelector(".gm-price").innerText),document.querySelector(".pay-for").style.display="block"}))}))})),window.initPoints=async function(){const e=c.email;try{const t=query(i,where("emailAdd","==",e)),a=await getDocs(t);let n;if(a.empty){const e=await addDoc(i,{emailAdd:e,points:5,claimed:!0,createdAt:Date.now()});n={id:e.id,points:5},u.pp.name=n.id?n.id:null}else{const t=a.docs[0];n={id:t.id,...t.data()},u.pp.name=n.id?n.id:null,n.claimed===!1&&(await updateDoc(doc(o,"points",n.id),{points:increment(5),claimed:!0}),n.points=(n.points||0)+5)}u.pp&&(u.pp.textContent=n.points,u.tpp.value=n.points),n.points>=30&&u.gm&&(u.gm.textContent=((n.points-5)/n.points).toFixed(1),u.tgm.value=((n.points-5)/n.points).toFixed(1))}catch(e){}};initPoints()}else window.location.href="/enter.html"})),document.getElementById("log").addEventListener("click",(()=>{signOut(n).then((()=>{window.location.reload()})).catch((e=>{}))})),window.addPoint=async function e(t){const a=doc(o,"points",u.pp.name);await updateDoc(a,{points:increment(t)}),initPoints()};function r(e,t){window.paypal&&document.querySelector("#paypal-button-container")&&paypal.Buttons({createOrder:(e,a)=>{return a.order.create({purchase_units:[{amount:{value:String(t)}}]})},onApprove:async(e,t)=>{const a=await t.order.capture();await addPoint(e),alert(`Thank you, ${a.payer.name.given_name}! You are credited with posting points and can now add your "Learn More" link.`)}}).render("#paypal-button-container")}
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+import { getFirestore, collection, addDoc, onSnapshot, getDocs, query, where, orderBy, deleteDoc, doc, updateDoc, increment } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+    const res = await fetch("/.netlify/functions/verifyFb");
+const config = await res.json();
+const app = initializeApp(config);
+    const auth = getAuth(app);
+    const db = getFirestore(app);
+    const pointCollection = collection(db, "points");
+    
+let currentUser = null;
+const userCard={
+  image: document.querySelector(".re-user-img"), 
+  name: document.querySelector(".re-user-name"), 
+  pp: document.querySelector(".pp-count"), 
+  gm: document.querySelector(".gm-count"), 
+  ex: document.querySelector(".c-ex"), 
+  ppIcon: document.querySelector(".icon.pp"), 
+  gmIcon: document.querySelector(".gem"),
+  getGem: document.querySelector(".gem"), 
+  getGift: document.querySelector(".gift"),
+  claimCard: document.querySelector(".gift-claim"),
+  claimGm: document.querySelector(".gm-claim"),
+  uname: document.getElementById("uname"), 
+  uem: document.getElementById("uem"), 
+  tpp: document.getElementById("tpp"), 
+  tgm: document.getElementById("tgm"), 
+}
+
+
+    onAuthStateChanged(auth, (user) => {
+        if(user){
+      currentUser = user;
+      
+      console.log(user.displayName);
+   
+    
+        const bgImage = currentUser.photoURL;
+            
+bgImage?userCard.image.style.cssText = `background: url(${bgImage}) no-repeat center;
+background-size: contain;`:userCard.image.textContent=`U`;
+                
+                currentUser.displayName?userCard.image.textContent=`${currentUser.displayName.slice(0,1)}`:userCard.image.textContent="U";
+            
+            
+             
+    userCard.name.textContent=`${currentUser.displayName? currentUser.displayName:`${currentUser.id?'user_'+currentUser.id.slice(0,5):'user_'}`}`;
+const giftCount = document.querySelector(".gift-count");
+userCard.uem.value=currentUser.email;
+            userCard.uname.value=currentUser.displayName;
+document.getElementById("refURL").value = `https://gemandpp.netlify.app/enter#ref?id=${encodeURIComponent(currentUser.email)}`;
+giftCount.innerText = userCard.claimCard.querySelectorAll(".card").length;
+userCard.getGift.addEventListener("click", (e)=>{
+  
+  userCard.claimCard.style.display='block';
+})
+userCard.gmIcon.addEventListener("click", (e)=>{
+  payFor(9,4.00);
+  document.querySelector(".pay-for").style.display = 'block';
+})
+userCard.claimCard.querySelectorAll(".card button").forEach(btn=>{
+  btn.onclick=function claimed(){
+  btn.style.opacity = "50%";
+  btn.textContent = "CLAIMED";
+  addPoint(1);
+  giftCount.innerText -=1;
+ btn.onclick=null;
+      setTimeout(()=>{
+  window.open("https://www.effectivecpmrate.com/uxx2gjrqc?key=76a628bbfc45e55ee2e31d6593b85938");
+      }, 300);
+}
+})
+
+const sellCard = document.querySelectorAll(".part-par");
+sellCard.forEach(card =>{
+  const leftTag = card.querySelector(".part-1");
+  const rightTag = card.querySelector(".part-2");
+  const innerColor = card.querySelector(".part-cover");
+  leftTag.addEventListener("click", (e)=>{
+    innerColor.style.marginLeft="0";
+    innerColor.style.borderRadius="10px 0 0 10px";
+  });
+  rightTag.addEventListener("click", (e)=>{
+    innerColor.style.marginLeft="45%";
+    innerColor.style.borderRadius="0 10px 10px 0";
+  });
+  const ppGet = card.querySelectorAll(".pp-get");
+  const ppPrice = card.querySelectorAll(".pp-price");
+  const gmGet = card.querySelectorAll(".gm-get");
+  const gmPrice = card.querySelectorAll(".gm-price");
+  const ppGetBtn = card.querySelectorAll(".get");
+  const gmGetBtn = card.querySelectorAll(".get-gm");
+  const buyGem = card.querySelector(".buy-gem");
+  
+  ppGetBtn.forEach(ppBtn=>{
+    ppBtn.addEventListener("click", (e)=>{
+    payFor(+ppBtn.parentElement.querySelector(".pp-get").innerText,ppBtn.parentElement.querySelector(".pp-price").innerText);
+    document.querySelector(".pay-for").style.display='block';
+    
+    });
+    
+  })
+  gmGetBtn.forEach(gmBtn => {
+      gmBtn.addEventListener("click", (e) => {
+        payFor(+gmBtn.parentElement.querySelector(".gm-get").innerText * 7, gmBtn.parentElement.querySelector(".gm-price").innerText);
+        document.querySelector(".pay-for").style.display = 'block';
+        
+      });
+  });
+ });
+ 
+window.initPoints = async function initPoints() {
+   const pointEm = currentUser.email;
+
+  try {
+    // query for existing record
+    const pq = query(pointCollection, where("emailAdd", "==", pointEm));
+    const snapshot = await getDocs(pq);
+
+    let userDoc;
+
+    if (snapshot.empty) {
+      // First time → create new doc with 5 points
+      const newDoc = await addDoc(pointCollection, {
+        emailAdd: pointEm,
+        points: 5,
+        claimed: true, // means already claimed initial reward
+        createdAt: Date.now()
+      });
+      userDoc = { id: newDoc.id, points: 5 };
+      userCard.pp.name=userDoc.id?userDoc.id:null;
+    } else {
+      // User already has a doc
+      const docSnap = snapshot.docs[0];
+      userDoc = { id: docSnap.id, ...docSnap.data() };
+
+      userCard.pp.name=userDoc.id?userDoc.id:null;
+      // If they don’t have points yet (claimed = false), grant once
+      if (userDoc.claimed === false) {
+        
+        const userRef = doc(db, "points", userDoc.id);
+        await updateDoc(userRef, {
+          points: (userDoc.points || 0) + 5,
+          claimed: true
+        });
+        userDoc.points = (userDoc.points || 0) + 5;
+      }
+    }
+
+    // update UI
+    
+    if (userCard.pp) {
+      userCard.pp.textContent = userDoc.points;
+      userCard.tpp.value=userDoc.points;
+    }
+    if (userDoc.points >= 30) {
+  if (userCard.gm) {
+    // Compute ratio: (points - 5) / points
+    let ratio = (userDoc.points - 5) / userDoc.points;
+    
+    // If result is negative, clamp it to 0
+    if (ratio < 0) {
+      ratio = 0;
+    }
+    
+    // Format to one decimal place
+    userCard.gm.textContent = ratio.toFixed(1);
+    userCard.tgm.value=ratio.toFixed(1);
+  }
+}
+  } catch (err) {
+    console.error("Error initializing points:", err);
+  }
+  
+}
+
+initPoints();
+    } 
+     else{
+      window.location.href =("/enter.html");
+     }
+}); 
+document.getElementById("log").addEventListener('click', () => {
+          signOut(auth).then(() => {
+            console.log("Logged out successfully");
+            window.location.reload();
+          }).catch((error) => {
+            console.error("Logout error:", error);
+          });
+        });
+
+window.addPoint = async function addPoint(pointTo) {
+  const uRef = doc(db, "points", userCard.pp.name);
+  await updateDoc(uRef, {
+    points: increment(pointTo),
+  });
+  initPoints();
+};
+
+function payFor(addPP, PPamt) {
+  if (window.paypal && document.querySelector("#paypal-button-container")) {
+    paypal.Buttons({
+      createOrder: (data, actions) => {
+        return actions.order.create({
+          purchase_units: [{ amount: { value: String(PPamt) } }]
+        });
+      },
+      onApprove: async (data, actions) => {
+        const details = await actions.order.capture();
+        
+        
+        
+        await addPoint(addPP);
+        alert(`Thank you, ${details.payer.name.given_name}! You are credited with posting points and can now add your "Learn More" link.`);
+      }
+    }).render('#paypal-button-container');}else{}}
